dd1040a54922ec2bee799b9742dfe7c51ef31815
AppiahEnoch <prignutt@gmail.com>
2023-06-06 12:25:33 -0700

    weeee

--- public_html/login/checkRegCode.php
+++ public_html/login/checkRegCode.php
@@ -7,9 +7,16 @@ $code ="PWReTUAH";
 $mobile="0549833352";
 
 
+
+
 try {
     $sql = "SELECT * FROM regCode WHERE code=? AND mobile=?";
-    $stmt = $conn->prepare($sql); 
+    $stmt = $conn->prepare($sql);
+    
+    if ($stmt === false) {
+        die ("Error: " . $conn->error);
+    }
+    
     $stmt->bind_param("ss", $code, $mobile);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -17,6 +24,8 @@ try {
         echo 1;
         exit();
     }
+    
+    
 } catch (\Throwable $th) {
    echo $th->getMessage();
 }
