dbb195b0d6de18f2ad5b25b06ec471402b49c848
AppiahEnoch <prignutt@gmail.com>
2023-06-06 12:31:14 -0700

    ssss

--- public_html/login/checkRegCode.php
+++ public_html/login/checkRegCode.php
@@ -3,30 +3,27 @@ require_once '../../vendor/autoload.php';
 include "../../config/config.php";
 
 
-$code ="PWReTUAH";
-$mobile="0549833352";
-
-
-
-
-try {
-    $sql = "SELECT * FROM regCode WHERE code=? AND mobile=?";
-    $stmt = $conn->prepare($sql);
-    
-    if ($stmt === false) {
-        die ("Error: " . $conn->error);
-    }
-    
-    $stmt->bind_param("ss", $code, $mobile);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while ($row = $result->fetch_assoc()) {
-        echo 1;
-        exit();
-    }
-    
-    
-} catch (\Throwable $th) {
-   echo $th->getMessage();
+$code ="";
+$mobile="";
+if (isset($_POST['code'])) {
+    $code = $_POST['code'];
+    $mobile = $_POST['mobile'];
 }
-  
+else{
+   echo false;
+    exit();
+}
+
+
+$sql = "SELECT * FROM regcode WHERE code=? AND mobile=?";
+$stmt = $conn->prepare($sql); 
+$stmt->bind_param("ss", $code, $mobile);
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()) {
+    echo 1;
+    exit();
+}
+
+echo 0;
+
