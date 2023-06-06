362297816329398bb40d2960ffe130a0b1cc00e3
AppiahEnoch <prignutt@gmail.com>
2023-06-06 12:20:43 -0700

    wwww

--- public_html/login/checkRegCode.php
+++ public_html/login/checkRegCode.php
@@ -7,16 +7,17 @@ $code ="PWReTUAH";
 $mobile="0549833352";
 
 
-
-$sql = "SELECT * FROM regCode WHERE code=? AND mobile=?";
-$stmt = $conn->prepare($sql); 
-$stmt->bind_param("ss", $code, $mobile);
-$stmt->execute();
-$result = $stmt->get_result();
-while ($row = $result->fetch_assoc()) {
-    echo 1;
-    exit();
+try {
+    $sql = "SELECT * FROM regCode WHERE code=? AND mobile=?";
+    $stmt = $conn->prepare($sql); 
+    $stmt->bind_param("ss", $code, $mobile);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        echo 1;
+        exit();
+    }
+} catch (\Throwable $th) {
+   echo $th->getMessage();
 }
-
-echo 0;
-
+  
